6542a29c114c28794bc26d6a9c5ce156224446be
kostapc <kostapc@gmail.com>
2019-01-14 17:47:33 +0300

    reformat to local formating rule

--- src/main/java/org/apache/zookeeper/inspector/ZooInspector.java
+++ src/main/java/org/apache/zookeeper/inspector/ZooInspector.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,63 +35,61 @@ import org.apache.zookeeper.inspector.manager.ZooInspectorManagerImpl;
 /**
  *
  */
-public class ZooInspector{
-	/**
-	 * @param args
-	 *            - not used. The value of these parameters will have no effect on the application
-	 */
-	public static void main(String[] args){
-		try{
-			// Dimension screenSize = getScreenResolution();
-			// int screenWidth = screenSize.width;
-			// int screenHeight = screenSize.height;
-			// System.out.println("screenWidth: " + screenWidth + ", screenHeight: " + screenHeight);
-			
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			JFrame frame = new JFrame("ZooInspector");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
-			// debug
-			// frame.setSize(screenWidth * 2 / 3, screenHeight);
-			// frame.setVisible(true);
-			
-			final ZooInspectorPanel zooInspectorPanel = new ZooInspectorPanel(new ZooInspectorManagerImpl());
-			frame.addWindowListener(new WindowAdapter(){
-				@Override
-				public void windowClosed(WindowEvent e){
-					super.windowClosed(e);
-					zooInspectorPanel.disconnect(true);
-				}
-			});
-			
-			frame.setContentPane(zooInspectorPanel);
-			frame.setSize(1024, 768);
-			// frame.setSize(screenWidth * 2 / 3, screenHeight);
-			frame.setVisible(true);
-		}
-		catch(Exception e){
-			LoggerFactory.getLogger().error("Error occurred loading ZooInspector", e);
-			JOptionPane.showMessageDialog(null, "ZooInspector failed to start: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-		}
-	}
-	
-	@SuppressWarnings("unused")
-	private static Dimension getScreenResolution(){
-		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		GraphicsDevice[] screenDevices = ge.getScreenDevices();
-		
-		for(int i = 0; i < screenDevices.length; i++){
-			System.out.println(screenDevices[i].getIDstring());
-			
-			DisplayMode dm = screenDevices[i].getDisplayMode();
-			int screenWidth = dm.getWidth();
-			int screenHeight = dm.getHeight();
-			
-			System.out.println("Cake: " + screenWidth + " " + screenHeight);
-			return new Dimension(screenWidth, screenHeight);
-		}
-		
-		return null;
-	}
-	
+public class ZooInspector {
+  /**
+   * @param args - not used. The value of these parameters will have no effect on the application
+   */
+  public static void main(String[] args) {
+    try {
+      // Dimension screenSize = getScreenResolution();
+      // int screenWidth = screenSize.width;
+      // int screenHeight = screenSize.height;
+      // System.out.println("screenWidth: " + screenWidth + ", screenHeight: " + screenHeight);
+
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      JFrame frame = new JFrame("ZooInspector");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      // debug
+      // frame.setSize(screenWidth * 2 / 3, screenHeight);
+      // frame.setVisible(true);
+
+      final ZooInspectorPanel zooInspectorPanel = new ZooInspectorPanel(new ZooInspectorManagerImpl());
+      frame.addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowClosed(WindowEvent e) {
+          super.windowClosed(e);
+          zooInspectorPanel.disconnect(true);
+        }
+      });
+
+      frame.setContentPane(zooInspectorPanel);
+      frame.setSize(1024, 768);
+      // frame.setSize(screenWidth * 2 / 3, screenHeight);
+      frame.setVisible(true);
+    } catch (Exception e) {
+      LoggerFactory.getLogger().error("Error occurred loading ZooInspector", e);
+      JOptionPane.showMessageDialog(null, "ZooInspector failed to start: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private static Dimension getScreenResolution() {
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    GraphicsDevice[] screenDevices = ge.getScreenDevices();
+
+    for (int i = 0; i < screenDevices.length; i++) {
+      System.out.println(screenDevices[i].getIDstring());
+
+      DisplayMode dm = screenDevices[i].getDisplayMode();
+      int screenWidth = dm.getWidth();
+      int screenHeight = dm.getHeight();
+
+      System.out.println("Cake: " + screenWidth + " " + screenHeight);
+      return new Dimension(screenWidth, screenHeight);
+    }
+
+    return null;
+  }
+
 }
